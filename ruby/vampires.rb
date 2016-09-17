278db8b55c5f25b65c047ede4465a8cc9d7cf2b7
Blakely Fountain <blake97@gmail.com>
2016-09-17 01:00:40 -0400

    set up data collection questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,18 @@
+puts "Hello. What is your name?"
+name = gets.chomp
+
+puts "Thank you, #{name}. Now, how old are you?"
+age = gets.chomp
+
+Puts "and hat year were you born?"
+year = gets.chomp
+
+Puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+Puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
+
+
+
 
