8bb78a24bc2c66cad970b51f298ad491a650f5ec
Blakely Fountain <blake97@gmail.com>
2016-09-25 15:57:31 -0400

    created nested datastructure for two boroughs within NYC

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+# I will be basing my data structure on a city block
+
+NYC_Boroughs = {
+  Manhattan: {
+    Neighborhood_info: {
+      contained_hoods: ['Upper East Side', 'Harlem', 'Chelsea'],
+      relative_health_index: 'Great',
+      bad_traffic_index: 'High',
+      streets_running_through: ["broadway","madison"],
+      Famous_landmarks: ['Central Park','Empire State Building','U.S. Intrepid']
+    },
+  },
+  Bronx:{
+    Neighborhood_info: {
+      contained_hoods: ['Bedford Park', 'Fordham', 'Norwood'],
+      relative_health_index: 'Poor',
+      bad_traffic_index: 'Low',
+      streets_running_through: ['Reeder St','Shepart Ave','Asylum Rd'],
+      Famous_landmarks:['Yankee Stadium','The Cloister'],
+    },
+  },
+}
+  
