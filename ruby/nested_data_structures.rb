2bc3c0a63db773108890b222254f2cf68bfbe84a
Blakely Fountain <blake97@gmail.com>
2016-09-25 16:21:13 -0400

    created ways of looking through the data at varying levels to show understanding of syntaxt and structure.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,23 +1,37 @@
 # I will be basing my data structure on a city block
 
 NYC_Boroughs = {
-  Manhattan: {
-    Neighborhood_info: {
+  manhattan: {
+    neighborhood_info: {
       contained_hoods: ['Upper East Side', 'Harlem', 'Chelsea'],
       relative_health_index: 'Great',
       bad_traffic_index: 'High',
       streets_running_through: ["broadway","madison"],
-      Famous_landmarks: ['Central Park','Empire State Building','U.S. Intrepid']
+      famous_landmarks: ['Central Park','Empire State Building','U.S. Intrepid']
     },
+    population: 'crazy big'
   },
-  Bronx:{
-    Neighborhood_info: {
+  bronx:{
+    neighborhood_info: {
       contained_hoods: ['Bedford Park', 'Fordham', 'Norwood'],
       relative_health_index: 'Poor',
       bad_traffic_index: 'Low',
       streets_running_through: ['Reeder St','Shepart Ave','Asylum Rd'],
-      Famous_landmarks:['Yankee Stadium','The Cloister'],
+      famous_landmarks:['Yankee Stadium','The Cloister'],
     },
+    population: 'big',
   },
 }
+
+NYC_Boroughs[:manhattan]
+NYC_Boroughs[:manhattan][:population]
+NYC_Boroughs[:manhattan][:neighborhood_info][:contained_hoods][1]
+NYC_Boroughs[:bronx][:neighborhood_info][:famous_landmarks][0]
+
+
+
+
+
+
+
   
