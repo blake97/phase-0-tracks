1f7c28fe9bb81b221768543def339539bc59e7d2
Blakely Fountain <blake97@gmail.com>
2016-09-27 09:50:12 -0400

    put program in a loop and assigned user intergace

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,59 +1,60 @@
+
+
  # ugly as hell method for swapping vowels only
- def vowels(string)
-    index = 0
-    until index == string.length
-      if string[index]== " "
-          string[index]=" " 
-        elsif string[index]== "a"
-          string[index]="e";
-      elsif string[index]== "e"
-          string[index]="i"
-        elsif string[index]== "i"
-          string[index]="o"
-        elsif string[index]== "o"
-          string[index]="u"
-      elsif string[index]== "u"
-        string[index]="a"
-        elsif string[index]== "A"
-          string[index]="E";
-      elsif string[index]== "E"
-          string[index]="I"
-        elsif string[index]== "I"
-          string[index]="O"
-        elsif string[index]== "O"
-          string[index]="U"
-      elsif string[index]== "U"
-        string[index]="A"  
-      else string[index]
+   def vowels(string)
+      index = 0
+      until index == string.length
+        if string[index]== " "
+            string[index]=" " 
+          elsif string[index]== "a"
+            string[index]="e";
+        elsif string[index]== "e"
+            string[index]="i"
+          elsif string[index]== "i"
+            string[index]="o"
+          elsif string[index]== "o"
+            string[index]="u"
+        elsif string[index]== "u"
+          string[index]="a"
+          elsif string[index]== "A"
+            string[index]="E";
+        elsif string[index]== "E"
+            string[index]="I"
+          elsif string[index]== "I"
+            string[index]="O"
+          elsif string[index]== "O"
+            string[index]="U"
+        elsif string[index]== "U"
+          string[index]="A"  
+        else string[index]
+        end
+        index += 1
       end
-      index += 1
     end
-  end
 
 
-puts "Hello, Agent. Give me your full name to create your spy alias."
+stop_word = false
+puts "Hello agent. Give me a full name to create your spy alias."
 
+until stop_word
 agent_name_input= gets.chomp
-
-#Name Processing: Split, Reverse, Rejoin ________
-
-#split name on space
-name_split= agent_name_input.split(' ')
-
-#reverse resulting array and rejoin on space
-reverse_name = name_split.reverse.join(' ')
-
-
-# <<<<<< ~~~~~~~~~~________ Name Processing: swap vowels ________~~~~~~~~~~  >>>>>> #
-
-#change vowels 
-vowels (reverse_name)
-
-#print new agent name
-puts reverse_name
+  
+  if agent_name_input == "quit"
+    stop_word= true
+  else
+    
+    #Name Processing: Split, Reverse, Rejoin ________
+    name_split= agent_name_input.split(' ')
+    reverse_name = name_split.reverse.join(' ')
+    
+    #change vowels & print name
+    vowels (reverse_name)
+    puts "\nYour secret name is #{reverse_name}. Please enter another name to create another alias for, or type 'quit' to exit the program."
+  end
+end
+   
  
- 
-
+puts "\n\nThank you for using the agent alias generator. Goodbye."
   
   
 
