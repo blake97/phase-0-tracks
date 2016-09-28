78f2b045be48bf753bf7897ab46226bc6d4a3a5d
Blakely Fountain <blake97@gmail.com>
2016-09-28 10:59:25 -0400

    studied and recreated bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -29,6 +29,36 @@ def fib_generator(total_fib_items)
    return start_array
 end
 fib_generator(100)
-
 # <<<<<< ~~~~~~~~~~________ Ruby Sorting Method ________~~~~~~~~~~  >>>>>> #
+
+#pseudocode
+ 
+ #1 create intake for the starting array to be sorted.
+ #2 iterate through the array in pairs, starting with the first two integers
+ #3 if the first integer of the first pair is the larger one, put that number in a new array, followed by the second integer. 
+ # if the second one is larger, put that one first in the new array, followed by the first integer.
+ #4 shift the reading frame down the array by two. complete the same process until reaching the end of the array.
+ #5 set the new array equal to the starting array
+ #6 continue steps 1-5 until there are no instances in which the second paired integer is smaller than the first integer.
+ #7 print the final new array
+
+def bubble_sort(number_list)
+  n= number_list.length
+  loop do
+    flipped = false
+    (n-1).times do |i|
+      if number_list[i] > number_list[i+1]
+       number_list[i], number_list[i+1] = number_list[i+1], number_list[i]
+       flipped = true
+      end
+    end
+    break if not flipped
+  end
+number_list
+end
+
+number_list= [2,32,34,6,239,3,45]
+bubble_sort(number_list)
+
+
 
