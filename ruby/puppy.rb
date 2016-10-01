3af2eb26067cf2964e0acbcfe28fdb571d1d8bf3
Blakely Fountain <blake97@gmail.com>
2016-10-01 19:33:47 -0400

    added a puppy class in pseudocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# <<<<<< ~~~~~~~~~~________ Release0 ________~~~~~~~~~~  >>>>>> #
+#designing puppy class
+
+# BREED ---------------
+# Golden Retriever
+
+# CHARACTERISTICS ---------------
+# Hair lengeth:
+# Hair Color:
+# Friendly:
+# Energy Level:
+# Color:
+# Adult Size:
+
+# Behavior ---------------
+# Retrieve
+# Lick
+# Run
+
+
+
+
+
+
