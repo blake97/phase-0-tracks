1de9adff7ecbec5d66fbd7ae5cdca6288dceb1ac
Blakely Fountain <blake97@gmail.com>
2016-10-01 21:04:05 -0400

    created methods in the puppy class for release0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,17 +13,56 @@ fido.instance_of?(Puppy)
 spot.instance_of?(Puppy)
 
 
-
+# Puppy class fetch method -------------------->>>
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
 end
-
-
 duchess.fetch("lamp")
 
-class
+
+# Puppy class speak method -------------------->>>
+class Puppy
+  def speak(number_woofs)
+   number_woofs=number_woofs.to_i
+     number_woofs.times {puts "Woof!"}
+  end
+end
+
+fido.speak(6)
+
+# Puppy class roll_over method -------------------->>>
+class Puppy
+  def roll_over
+    puts "**rolls over**"
+  end
+end
+
+fido.roll_over
+
+
+# Dog years method -------------------->>>
+class Puppy
+  def dog_years(human_age)
+    dog_equivalant= human_age*7
+    puts "In dog years, that's #{dog_equivalant}!"
+  end
+end
+
+puts "In human years, how many years old is your dog?"
+human_age=gets.to_i
+
+fido.dog_years(human_age)
+
+# Dog plays guitar method -------------------->>>
+class Puppy
+  def guitar
+    puts "***Tocando la guitarra***"
+  end
+end
+fido.guitar
+
+
+
