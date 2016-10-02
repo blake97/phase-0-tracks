e620ec84acdcda1292bc3d477e479b42dbd9d4d8
Blakely Fountain <blake97@gmail.com>
2016-10-02 11:06:01 -0400

    added 11 initializations of the Santa class with varying attributes_did this via looping through two arrays and putting them together as pairs in block code which was then fed to the santa initializing method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,23 +2,44 @@
 ####### METHODS/CLASSES ##########
 
 class Santa
+
+
+  #initialize new instance of Santa
+  def initialize(gender,ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+
+  end
+
+
   # santa speak method "Ho, ho, ho! Haaaappy holidays!"
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
+
+
   #eat_milk_and_cookies method
   def eat_milk_and_cookies
     puts "*** Santa eats milk and cookies ***"
-  end
-  #initialize new instance of Santa
-  def initialize
-    puts "Initializing Santa instance..."
   end
 end
 
 ########## DRIVER CODE for Release 0 ############
 #testing new instance
 
-Santa.new
+santas = []
+
+new_santa_genders =["gender questioning","transgender person","transgender female","transgender woman","transgender man","neutrois",
+                    "male", "other", "male", "female", "female"]
+
+new_santa_ethnicities =["polynesian","black","white", "north african", "south american", "alaska native", "sub-saharan african","white", "hispanic mestizo", "asian american", "asian american"]
 
 
+new_santa_genders.length.times do |i|
+  santas << Santa.new(new_santa_genders[i],new_santa_ethnicities[i])
+  puts "There are now #{santas.length} Santa instances in the array"
+  puts "----"
+end
