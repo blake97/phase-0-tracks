a17cc2ed11e3ad6dbe00ea35c150843f36fac2c0
Blakely Fountain <blake97@gmail.com>
2016-10-02 20:16:42 -0400

    completed the shout document by rewriting modules in mixin format and calling them on a new instance within classes these modules were included in.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,19 +1,54 @@
+# ###################### Method Code #######################
+
+# module Shout
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words + "!!!!!!!!!!!!!" + " :D"
+#   end
+
+# end
+
+# ###################### Driver Code #######################
+
+# require_relative 'shout'
+# Shout.yell_angrily("SHIT")
+# Shout.yelling_happily("YEAAAHHHHH")
+
+
+# ---------------------------- Release 3 --------------------------------
+
 ###################### Method Code #######################
 
+#Mix-in Module#
 module Shout
-
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words + "!?!?!" + " :("
   end
 
-  def self.yelling_happily(words)
-    words + "!!!!!!!!!!!!!" + " :D"
+  def yelling_happily(words)
+    words + "!!!!!!!!!!!!!!!!!!" + ":D"
   end
-
 end
 
-###################### Driver Code #######################
+#classes with modules included#
+class Father
+  include Shout
+end
 
-require_relative 'shout'
-Shout.yell_angrily("SHIT")
-Shout.yelling_happily("YEAAAHHHHH")
+class Coach
+  include Shout
+end
+
+
+chesster_daniel_fountain=Father.new
+p chesster_daniel_fountain.yell_angrily("YOU SPENT HOW MUCH")
+p chesster_daniel_fountain.yelling_happily("IM RETIRING IN 2.5 MONTHS")
+
+
+coach_james=Coach.new
+p coach_james.yell_angrily("WHAT WERE YOU THINKING")
+p coach_james.yelling_happily("WE WON THE CHAMPIONSHIP")
