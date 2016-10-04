8fbc90445f5feb140978c661115ca1ed73ca9bdd
Blakely Fountain <blake97@gmail.com>
2016-10-03 20:41:01 -0400

    completed first draft and am afraid to run it. first draft has many questions and was mostly me just writing what i thought, as i thought it. still, there's a reason for everythign there, and i can definitely talk my way through it to whomever is so unlucky to pass its pixels.

--- ruby/game/word_guessing_game.rb
+++ ruby/game/word_guessing_game.rb
@@ -2,7 +2,6 @@
 ###### 1) One user can enter a word (or phrase, if you would like your game to
 #         support that), and another user attempts to guess the word.
 #------------
-#
 # word_to_guess
 #
 ###### 2) Guesses are limited, and the number of guesses available is related
@@ -10,12 +9,9 @@
 #------------
 # guesses_allowed (three less than word_to_guess.length)
 #
-
 ###### 3) Repeated guesses do not count against the user.
 #------------
 # if someone guesses the same letter twice, there's no penalty
-
-
 
 ###### 4) The guessing player receives continual feedback on the current state
 #         of the word. So if the secret word is "unicorn", the user will start
@@ -36,13 +32,14 @@
 ############################################## Class Code #######################
 class Wordgame
   attr_reader :game_board :game_outcome :num_guesses_allowed
-  attr_accessor :word_to_guess
+  attr_accessor :word_to_guess :guessed_letters
 
 
   def initialize
     @word_to_guess = nil
     @num_guesses_allowed = (@word_to_guess.length)-3
     @game_board = @word_to_guess.lenth {|x| x*"_"}
+    @guessed_letters = []
   end
 
 
@@ -51,29 +48,36 @@ class Wordgame
   end
 
 
-  def current_try (letter_guess)
+  def current_try (letter_guess,current_guess)
     until current_guess == @num_guesses_allowed
-      if @letters.times.map! do |x| include?'#letter_guess'
-          @game_board[x] = current_guess
+      @guessed_letters << letter_guess
+      if @letters.each.times do |letter,index| letter include?'#letter_guess'
+          @letters[index].map! = current_guess
+          @game_board= letters.join('')
           puts "You found a letter! #BOOM!!! Here's your updated Board: #{@game_board}"
+
           if @game_board = word_to_guess
             puts "YOU WIN! You so smaaaarrrrttttt!"
             exit
           end
         end
+
       else @letters.each {|x| !include?'#letter_guess'}
         puts "Nope... not a letter. Womp. The current game board is still #{{@game_board}}"
         puts "Please select another letter. This is #{current_guess} of #{@num_guesses_allowed}"
-        current_guess +=1
       end
+    end
+    while guessed_letters !include?'#{letter_guess}'
+      current_guess +=1
     end
     puts "You loose. Peace out... oh, and the word was @word_to_guess, dummy."
     exit
   end
+
 end
 
 
-############################################## Driver Code #######################
+############################################## Driver Code #######################      
 puts "Welcome to the Game!"
 game = Wordgame.new
 
@@ -88,6 +92,3 @@ puts "Player 1. Please make your first guess and hit enter."
 current_guess = 1
 letter_guess = gets.chomp.to_s
 letter_guess.current_try(letter_guess)
-
-
-
