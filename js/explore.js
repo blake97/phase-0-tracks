31b74696c7dd87756c19bfa214d9379d7fd74d19
Blakely Fountain <blake97@gmail.com>
2016-10-12 11:28:26 -0400

    created javascript to run a reversal funtion on a string, then had that string print out on the condition that num = 1

---
+++ js/explore.js
@@ -0,0 +1,31 @@
+// # <<<<<< ~~~~~~~~~~________Pseudocode________~~~~~~~~~~  >>>>>> #
+// step 1: take some input as a tring.
+// step 2: create a new empty container string for the reversed output. 
+// step 3: copy the last character of input string and add 
+//         to the empty output string
+// Step 4: move backward down the input string by adding -1
+// step 5: complete steps 1 - 4 until the string has been covered.
+// step 6: return the final value
+
+
+function reverse_string(input) {
+  var output_string="";
+  for(var i=input.length -1; i >= 0;i--){
+    output_string+=input[i];
+  }
+    return output_string;
+}
+
+
+
+
+// #### driver code ####
+reverse_string('hello');
+
+
+var num=1
+if (num == 1){
+  console.log (reverse_string('hello'));
+
+
+
