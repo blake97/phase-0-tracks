27f7a7b4a03ed35c458fe11fe8af2bbe1f2b7725
Blakely Fountain <blake97@gmail.com>
2016-10-15 11:11:09 -0400

    Created Method dective Jackrabbit file.

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+// declaring two arrays
+var colors = ["blue", "green","red","orange"];
+var names = ["Ed", "Hubert", "Chesterfield", "Daisy"];
+
+// adding a value to each array
+colors.push("hot pink");
+names.push("Jerald");
+
+
+var horses_and_colors {}
+
