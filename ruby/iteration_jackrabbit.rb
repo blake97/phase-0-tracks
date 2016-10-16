83c38310eab798ad7627cdf4637a8c04d2d6a479
Blakely Fountain <blake97@gmail.com>
2016-10-16 18:11:20 -0400

    finished up with release 2. Still not sure what question 4 is asking for...

--- ruby/iteration_jackrabbit.rb
+++ ruby/iteration_jackrabbit.rb
@@ -47,3 +47,108 @@ end
 
 puts "\n\nedited actor names in place using .map!"
 puts actors_arr
+
+
+# <<<<<< ~~~~~~~~~~________Release 2________~~~~~~~~~~  >>>>>> #
+
+
+arr = [1,4,3,9,5,5,7,8,4,10,11]
+name_age = {
+james: 3,
+zach: 4,
+evalyn: 2,
+cody: 8,
+martha: 1,
+}
+
+# A method that iterates through the items, deleting any that meet a certain condition 
+# (for example, deleting any numbers that are less than 5).
+
+arr.delete_if {|number| number <=7}
+p arr
+
+name_age.delete_if {|name, age| age >= 5}
+p name_age  
+
+
+
+
+
+# A method that filters a data structure for only items that do satisfy a certain 
+# condition (for example, keeping any numbers that are less than 5).
+
+arr = [1,4,3,9,5,5,7,8,4,10,11]
+name_age = {
+james: 3,
+zach: 4,
+evalyn: 2,
+cody: 8,
+martha: 1,
+}
+
+arr.keep_if {|num| num <3}
+puts arr
+
+name_age.keep_if {|name, age| age == 4}
+p name_age
+
+
+
+# A different method that filters a data structure for only items satisfying a 
+# certain condition -- Ruby offers several options!
+
+arr = [1,4,3,9,5,5,7,8,4,10,11]
+name_age = {
+james: 3,
+zach: 4,
+evalyn: 2,
+cody: 8,
+martha: 1,
+}
+
+arr.select! {|number| number >5}
+
+name_age.select! {|name, age| name != :cody} 
+
+
+# A method that will remove items from a data structure until the condition in the 
+# block evaluates to false, then stops (you may not find a perfectly working option 
+# for the hash, and that's okay).
+
+puts "\n\n Original Array."
+arr = [1,4,3,16,4,5,5,6,7,8,4,10,11]
+puts arr
+
+puts "Sort then drop numbers while the number is less than 5. "
+arr.sort!.take_while {|number|number< 5}
+puts arr.sort!.take_while {|number|number< 5}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
