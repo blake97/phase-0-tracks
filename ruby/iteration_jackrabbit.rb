62e3a07c86ff557ead9a47a578885e2aba4cce90
Blakely Fountain <blake97@gmail.com>
2016-10-16 16:53:40 -0400

    added iterations both the array and hash with .each, .map, and .map bang  - note that the hash was not iterated with .map bang

--- ruby/iteration_jackrabbit.rb
+++ ruby/iteration_jackrabbit.rb
@@ -1,13 +1,49 @@
 #declare an array and a hash
-
-actors_arr = ["Haley Bennet", "Ben Stiller", "Simone Missick", "Bernie Mac", "Eva Green"]
+actors_and_friends=[]
+new_units_of_measurement = {}
+actors_arr = ["haley bennet", "ben stiller", "simone missick", "bernie mac", "eva green"]
 units_of_measurement_hash = {
-length: "inches",
-volume: "fluid ounces",
-weight: "pounds & ounces",
-money: "dollar & cents",
-time: "seconds"
+  length: "inchz",
+  volume: "fluid ounces",
+  weight: "pound & ounces",
+  money: "dollar & cents",
+  time: "seconds"
 }
 
-#iteration with .each
-actors_arr.each
+puts "Iteration through array with .each"
+#iteration of array with .each
+actors_arr.each do |actor_name|
+puts "#{actor_name} is a pretty famous actor... just sayin."
+end
+
+#iteration of hash with .each
+puts "\n\niteration through hash with .each"
+units_of_measurement_hash.each do |measurement_type, units|
+ puts "In America, we measure #{measurement_type} in #{units}."
+end
+
+#iteration of array with .map, using it to create a new array
+actors_arr.map do |actor|
+  actors_and_friends << "#{actor} and friends"
+  actors_and_friends
+end
+puts "\n\nnewly created array using map, based off of original actors array:"
+puts actors_and_friends
+
+
+units_of_measurement_hash.map do |measurement_type, units|
+  new_units_of_measurement[measurement_type]=units.chop
+end
+
+puts "\n\nnewly created hash using .map and the origianal units_of_measurement_hash (removed 's' from end)."
+puts new_units_of_measurement
+
+
+#map! on array
+
+actors_arr.map! do |actor|
+  actor.upcase
+end
+
+puts "\n\nedited actor names in place using .map!"
+puts actors_arr
