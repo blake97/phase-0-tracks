a0e0c7da3bc63586731e5c19b883b0352e7446ae
Blakely Fountain <blake97@gmail.com>
2016-10-16 10:08:25 -0400

    set the form up to deliver intake responses to the newly created hash 'client_info'. demonstrated both symbol and hash rocket notation.

--- ruby/hashes_jackrabbits.rb
+++ ruby/hashes_jackrabbits.rb
@@ -11,16 +11,29 @@ client_name = gets.chomp
 puts "How old is #{client_name}?"
 client_age = gets.chomp.to_i
 
+puts "What style of decore is #{client_name} going for?"
+decore_style = gets.chomp
+
 puts "How many children does #{client_name} have?"
 client_children = gets.chomp.to_i
 
 puts "Does #{client_name} like the color 'new grass green?'?"
 likes_grass_green? = gets.chomp
 
-puts "What style of decore is #{client_name} going for?"
-decore_style = gets.chomp
 
 
+
+
+# put form information into a hash. NOTE. no real use for hash 
+# rocket notation here, but I'm including it as example.
+
+client_info {
+client_name: client_name,
+client_age: client_age,
+decore: decore_style,
+"Number of Children" => client_children,
+"Likes New Grass Green?" => likes_grass_green
+}
 
 
 
