a5dd2bf2389152b054317cbac3ba16fcaa079d9d
Blakely Fountain <blake97@gmail.com>
2016-10-16 09:54:36 -0400

    created user intake form that will convert input to apropriate datatype. note that i have not taken measures to ensure proper input type. possible future feature to add.

---
+++ ruby/hashes_jackrabbits.rb
@@ -0,0 +1,27 @@
+Pseudocode
+
+List various items to collect of differnt datatypes.
+
+
+
+#collect form information.
+puts "Please enter the name of your client."
+client_name = gets.chomp
+
+puts "How old is #{client_name}?"
+client_age = gets.chomp.to_i
+
+puts "How many children does #{client_name} have?"
+client_children = gets.chomp.to_i
+
+puts "Does #{client_name} like the color 'new grass green?'?"
+likes_grass_green? = gets.chomp
+
+puts "What style of decore is #{client_name} going for?"
+decore_style = gets.chomp
+
+
+
+
+
+
