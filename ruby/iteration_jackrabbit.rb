c6f96696d998ece4b0f67100767a42139e190e3f
Blakely Fountain <blake97@gmail.com>
2016-10-16 15:39:30 -0400

    initial commit with declairation of one array and one hash.

---
+++ ruby/iteration_jackrabbit.rb
@@ -0,0 +1,13 @@
+#declare an array and a hash
+
+actors_arr = ["Haley Bennet", "Ben Stiller", "Simone Missick", "Bernie Mac", "Eva Green"]
+units_of_measurement_hash = {
+length: "inches",
+volume: "fluid ounces",
+weight: "pounds & ounces",
+money: "dollar & cents",
+time: "seconds"
+}
+
+#iteration with .each
+actors_arr.each
