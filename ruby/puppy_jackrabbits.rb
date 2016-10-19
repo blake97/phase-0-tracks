f763f1841606eb74dbd84cb093eb0f179b1516c7
Blakely Fountain <blake97@gmail.com>
2016-10-19 09:42:15 -0400

    created module file, and added to it thepuppy class as well as the first three instances of puppy.

---
+++ ruby/puppy_jackrabbits.rb
@@ -0,0 +1,34 @@
+# <<<<<< ~~~~~~~~~~________ Release 0________~~~~~~~~~~  >>>>>> #
+
+# -------- characteristics --------
+# hair_length
+# relative_size
+# temperment
+# color
+
+# -------- behaviors --------
+# fetch
+# bark
+# poop
+# eat
+
+# <<<<<< ~~~~~~~~~~________ Release 1 ________~~~~~~~~~~  >>>>>> #
+class Puppy
+end
+
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+# spot.class
+# duchess == fido
+# fido.instance_of?(Array)
+# fido.instance_of?(Puppy)
+# spot.play_dead
+
+
+# <<<<<< ~~~~~~~~~~________ Release 2 ________~~~~~~~~~~  >>>>>> #
+
+
+
