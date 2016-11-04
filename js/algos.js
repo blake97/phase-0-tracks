228815a12a027585d151a5c0a17356a0c63f4d15
Blakely Fountain <blake97@gmail.com>
2016-11-03 22:57:53 -0400

    initial commit of empty file.

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+///////// ####### Release 1: Find a Key-Value Match ############################
+// write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+
+// 
