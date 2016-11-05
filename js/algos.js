6932a71c604b775f0303e8c81f008707972ac051
Blakely Fountain <blake97@gmail.com>
2016-11-04 20:25:45 -0400

    completed release zero. After I started down the wrong path, I refocused on approaching it fom a different angle. Nothign fancy, just un-obvious logic. The method carries the first word as it loops until another word is encountered. If that word's length is greater, the function replaces the shorter string with the longer one. THis continues until the longest string is left over at the end.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,28 @@
+// Release 0 function that takes an array of words or phrases and returns the longest word or phrase in the array.
+// pseudocode
+// 1) create an array full of items to be measured (strings)
+// 2) we could start at the first item, and then continue comparing subsequent items to that first item
+
+var wordArray = ["hello", "I said HELLO!", "THIS IS SUCH A LONG PHRASE"]
+
+function return_longest(wordArray) {
+
+    var longest_item = "";           // I need to cerate the variable I'll be storing the longest string in,   and then calling to be printed out. As for value, I'll use "0", so anything is guaranteed to be higher than it is. We'll continue looping until the highest 
+
+    for (i = 0; i < wordArray.length; i++) {
+        if (wordArray[i].length >= longest_item.length) {
+            longest_item = wordArray[i];   
+        }
+    }
+    return longest_item;
+}
+
+return_longest(wordArray);
+
+var wordArray = ["ppppppppppppppppp", "ppppp", "p"]
+
+
 ///////// ####### Release 1: Find a Key-Value Match ############################
 // write a function that takes two objects and checks to see if the objects share at least one key-value pair.
 
-// 
+//  
