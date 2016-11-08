6526b229f6287d11dbd0d84cea01a49bbc134102
Blakely Fountain <blake97@gmail.com>
2016-11-08 17:00:00 -0500

    horrible git workflow, but I'm honestly just tryign to get this done to catch up with everyone! I created a very light database meant to search through artwork for sale across various collections. there were 4 tables total. I started working on driver code, but i quickly realized i made this FAR more complicated that it should have been... it's crazy how the simplest of ideaas translates into SO MUCH CODING! Anyway, the one extra fucntion I created was where the user enters in their income. Future release would compare income level to price level and determine if the user should be buing at the price they are... if not, it owuld advise them to save their moeny lol. Next, I finished preliminary code of a search art function that asks for parameters and then searches a joined database on those given parameters and then returns them.

---
+++ databases/Art_project/art_program.rb
@@ -0,0 +1,155 @@
+# this project is somewhat mirroring the website plan i've laid out for this web developer group to complete... i submitted to the project before i started dev bootcamp, so this is really nice to see what they do to make my ideas come to fruition.
+
+
+# set up the database and tables
+##########################################################################
+
+require 'sqlite3'
+
+db = SQLite3::Database.new("artbase.db")
+db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS collections(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  relative_price INT
+);
+SQL
+
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS art_pieces(
+  id INTEGER PRIMARY KEY,
+  name varchar(255),
+  style_id INT,
+  medium_id INT,
+  collection_id INT,
+  FOREIGN KEY (style_id) REFERENCES styles(id),
+  FOREIGN KEY (medium_id) REFERENCES media(id),
+  FOREIGN KEY (collection_id) REFERENCES collections(id)
+);
+SQL
+
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS media(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255)
+);
+SQL
+
+create_table_cmd = <<-SQL
+CREATE TABLE IF NOT EXISTS styles(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255)
+);
+SQL
+
+db.execute(create_table_cmd)
+
+def add_collection(db, name, relative_price)
+  db.execute("INSERT INTO collections (name, relative_price) values (?,?)", [name, relative_price])
+end
+
+
+def add_style(db, name)
+  db.execute("insert into styles (name) values (?)", [name])
+end
+
+
+def add_medium(db, name)
+  db.execute("insert into media (name) values (?)", [name])
+end
+
+
+def add_artpiece(db, name, style_id, medium_id, collection_id)
+  db.execute("INSERT INTO art_pieces (name, style_id, medium_id, collection_id) VALUES (?,?,?,?)", [name, style_id, medium_id, collection_id])
+end
+
+
+#adding some collections #############################################
+add_collection(db, "White House Collection", 5) #<== relative price is number dollar signs out of 5 dollar signs
+add_collection(db, "Martha Stewart Collection", 2)
+add_collection(db, "dollar store", 1)
+add_collection(db, "pier 1", 2)
+
+#adding some styles ##################################################
+add_style(db, "American Classic")
+add_style(db, "pop culture")
+add_style(db, "Geometric")
+
+#adding some media ##################################################
+add_medium(db, "oil")
+add_medium(db, "watercolor")
+add_medium(db, "digital")
+
+#adding art pieces ###################################################
+add_artpiece(db, "Portrait of George Washington", 1, 1, 2)
+add_artpiece(db, "beautiful picture", 2, 3, 1)
+add_artpiece(db, "ARTE", 5,2,3)
+
+
+# Join tables ##########################################################################
+
+
+
+# Driver Code ##########################################################################
+puts "Hello, what's your annual income?"
+income = gets.to_i
+valid_income = false
+
+until valid_income
+  if income < 20,000
+    buy_power = 1
+    valid_income = true
+  elsif income <40,000
+    buy_power=2
+    valid_income = true
+  elsif income < 60,000
+    buy_power = 3
+    valid_income = true
+  elsif income < 80,000
+    buy_power = 4
+    valid_income = true
+  elsif income >=120,000
+    buy_power = 5
+    valid_income = true
+  else
+    puts "please enter your income."
+    valid_income = false
+    income = gets.to_i
+  end
+end
+
+def search_art()
+  puts "What medium?"
+  medium_in = gets.chomp.downcase
+  if medium_in == "oil"
+    medium_id = 1
+    art = db.execute("SELECT * FROM art_pieces where ")
+  elsif medium_in == "watercolor"
+    medium_id = 2
+  elsif medium_in == "digital"
+    medium_id = 3
+  else
+    puts "I'm sorry, we don't have any #{medium_in} works."
+  end
+
+  db.execute (SELECT art_pieces.name,
+              collections.name,
+              media.name,
+              styles.name,
+              collections.relative_price
+             
+       FROM   collections,
+              art_pieces,
+              media,
+              styles
+
+       Where  medium.id == medium_id
+       
+       JOIN   art_pieces
+              ON art_pieces.style_id = styles.id,
+              AND art_pieces.business_id = businesses.id,
+              AND art_pieces.collection_id = collections.id;
+        )
+end
--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -1,44 +1,93 @@
 # OPERATION KITTEN EXPLOSION!
 
+
+# -- Setup -------------------------------------------------------------------------
 # talk about Object Relational Mapping (ORM)
 
 # require gems
 require 'sqlite3'
 require 'faker'
 
-# create SQLite3 database
+# create SQLite3 database and store it in a variable (white db below)
 db = SQLite3::Database.new("kittens.db")
-db.results_as_hash = true
+# database should be able to run ls in terminal to see the database or not...  don't use sqlite3 to do this bc if it's not there, the program would create it, adn give us a false positive
+# git commit here!
+
+db.results_as_hash = true  # this impact how the gem on line 17 returns our data. So, instead of the nested array structure, we get an array of hashes instead of an array of arrays
+
+#if we want to call a method that iterates through kittens and returns some data, we can perform the below code. 
+# kittens.each do |kitten|
+#   puts "{kitchen['name']} is #{kitten['age']}"
+# end
 
 # learn about fancy string delimiters
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
+create_table_cmd = <<-SQL        # here, doesn't matter what won't show up in the string text below... Here, we used SQL because that's what we're tryign to work with here. NOTE that the code below IS ruby. It is NOT sql! it's abother way of declairing a string, the same was as  " str = "this is a string" is made
+  CREATE TABLE IF NOT EXISTS kittens( # IF NOT EXISTS is sql language. If we run kitten_maker twice, we'd get an error saying that the tables already exist... to get around that, we simply put this in. it won't try to make the kittens table since it's already been done. 
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     age INT
   )
-SQL
+SQL               # here,  type it again, and this closes it out.
 
 # create a kittens table (if it's not there already)
-db.execute(create_table_cmd)
+db.execute(create_table_cmd)  # here, we're telling ruby to execute the code from lines 18 - 24
 
-# add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+--  -------------------------------------------------------------------------
+# add a test kitten - the below code would run every time we run the entire ruby file. the problem with that is we would get duplicates, since our program is now persistent. So, after running it, we'll comment it out to prevent further duplicate entries.
+
+# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)") 
 
 # add LOOOOTS of kittens!
 # so. many. kittens. 
 #KittenExplosion
+
+
 def create_kitten(db, name, age)
-  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
+  db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])# the question marks are the place holders for data that gets dropped in. We don't use interpolation becasue they'd each need quotes around it, but also, SQL Injection (a security attack) is possible. So, it's bad practice to interpolate data into sql. Instead, use ? as a placeholder. First argument is the long sql "insert into kittens" in the second argument, provide the values in the order you want them to appear ([name], [age])
 end
 
+#now, we want to make LOTS of kittens... defalt age is 0 years old(kittens)
 10000.times do
   create_kitten(db, Faker::Name.name, 0)
 end
 
+# the below is to just explore the data we created for verification purposes... if we don't want it to run, we just comment it out.
+
 # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
+
+kittens = db.execute("SELECT * FROM kittens")
+puts kittens.class # because it's good to just go ahead and call on the data since some of the stuff we work with may have proprietary datatypes or classes.
+p kittens #to return the data.
+
 # kittens.each do |kitten|
 #  puts "#{kitten['name']} is #{kitten['age']}"
 # end
+
+
+
+
+SELECT users.first, 
+       users.last, 
+       businesses.name, 
+       reviews.stars, 
+       reviews.comment 
+FROM   users, 
+       businesses 
+       JOIN reviews 
+        ON reviews.user_id = users.id 
+        AND reviews.business_id = businesses.id; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
