c2f9e2c595894274ce57c76334c07cd511d4b338
Blakely Fountain <blake97@gmail.com>
2016-11-13 19:04:06 -0500

    couldn't get the image to appear after clicking, so i'm jsut leaving it at clicking to change the background.

--- web_dev/client_side_js/javascript.js
+++ web_dev/client_side_js/javascript.js
@@ -1,1 +1,39 @@
-console.log ("The script is working!");
+
+var background = document.getElementsByTagName("body");
+background = background[0];
+background.addEventListener("click", changeBackgroundColor);
+
+
+function changeBackgroundColor(event) {
+  
+event.target.style.backgroundColor = "red";
+}
+
+var background = document.getElementsByTagName("body");
+background = background[0];
+background.addEventListener("click", changeBackgroundColor);
+
+
+
+// -------
+function showAfter(event) {
+  if (false == $(after).is('hidden')) {
+    $(after).show(250)}
+  else {
+      $(after).hide 250;
+    }
+  }
+
+
+// or this way? 
+
+event.target.style.visibility = "visible";
+}
+
+var aftermath = document.getElementById("after");
+aftermath.addEventListener("click", showAfter);
+
+
+// I can't get this to work at all. It's driving me mad. 've been here hours, but it's rabithole after rabithole.
+
+
