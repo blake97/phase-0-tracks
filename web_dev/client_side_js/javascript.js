1501afdbdc87dce7d0aba326c3a08c7a437d15b1
Blakely Fountain <blake97@gmail.com>
2016-11-13 19:21:10 -0500

    edit

--- web_dev/client_side_js/javascript.js
+++ web_dev/client_side_js/javascript.js
@@ -9,31 +9,31 @@ function changeBackgroundColor(event) {
 event.target.style.backgroundColor = "red";
 }
 
-var background = document.getElementsByTagName("body");
-background = background[0];
+var background = document.getElementById("body");
+
 background.addEventListener("click", changeBackgroundColor);
 
 
 
-// -------
-function showAfter(event) {
-  if (false == $(after).is('hidden')) {
-    $(after).show(250)}
-  else {
-      $(after).hide 250;
-    }
-  }
+// // -------
+// function showAfter(event) {
+//   if (false == $(after).is('hidden')) {
+//     $(after).show(250)}
+//   else {
+//       $(after).hide 250;
+//     }
+//   }
 
 
-// or this way? 
+// // or this way? 
 
-event.target.style.visibility = "visible";
-}
+// event.target.style.visibility = "visible";
+// }
 
-var aftermath = document.getElementById("after");
-aftermath.addEventListener("click", showAfter);
+// var aftermath = document.getElementById("after");
+// aftermath.addEventListener("click", showAfter);
 
 
-// I can't get this to work at all. It's driving me mad. 've been here hours, but it's rabithole after rabithole.
+// // I can't get this to work at all. It's driving me mad. 've been here hours, but it's rabithole after rabithole.
 
 
